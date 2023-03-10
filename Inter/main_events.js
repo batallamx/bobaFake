1f578d3d9966f8af4db681b593c1ec1c0f20f387
00Kique <124222443+00Kique@users.noreply.github.com>
2023-03-09 18:50:36 -0600

    fecha

--- Inter/main_events.js
+++ Inter/main_events.js
@@ -5,3 +5,9 @@ $(document).ready(function(){
     speed: 2000
   });
 });
+
+$(document).ready(function(){
+  var today = new Date();
+  var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+  document.querySelector('.date').innerHTML = today.toLocaleDateString('es-ES', options);
+});
