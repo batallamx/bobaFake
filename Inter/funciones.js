918ceee211c76d9b9c1702ef933e6837430fea1b
Angel David <david@batalla.mx>
2023-03-11 20:25:26 -0600

    Formulario

--- Inter/funciones.js
+++ Inter/funciones.js
@@ -1,61 +1,60 @@
-var formulario = document.getElementById('form'),
-    nombre = form.nombre,
-    correo = form.correo,
-    genero = form.genero,
-    terminos = form.terminos,
-    error = document.getElementById('error');
+var Formulario = document.getElementById("form");
+var nombre = Formulario.nombre,
+	correo = Formulario.correo,
+	genero = Formulario.genero,
+	terminos = Formulario.terminos;
+var error = document.getElementById("error");
 
-function validarName(e){
-    if(nombre.value== ''||nombre.value==null){
-        error.style.display='block';
-        error.innerHTML += '<li> Complete name </li>';
-        console.log('Please add a name');
+function validarName(e) {
+	if (nombre.value == "" || nombre.value == null) {
+		error.style.display = "block";
+		error.innerHTML += "<li> Complete name </li>";
+		console.log("Please add a name");
 
-        e.preventDefault();
-    } else{
-        error.style.display='none';
-    }
+		e.preventDefault();
+	} else {
+		error.style.display = "none";
+	}
 }
 
-function validarEmail(e){
-        if(correo.value == '' || correo.value == null){
-        error.style.display='block';
-        error.innerHTML += '<li>Complete email</li>';
-        console.log('Please add an email');
+function validarEmail(e) {
+	if (correo.value == "" || correo.value == null) {
+		error.style.display = "block";
+		error.innerHTML += "<li>Complete email</li>";
+		console.log("Please add an email");
 
-        e.preventDefault();
-    } else{
-        error.style.display='none';
-    }
+		e.preventDefault();
+	} else {
+		error.style.display = "none";
+	}
 }
-function validarGender(e){
-    if(genero.value == '' || genero.value == null){
-        error.style.display='block';
-        error.innerHTML+='<li> Select gender </li>';
-        console.log('Please select gender');
-        e.preventDefault();
-    } else{
-        error.style.display='none';
-    }
+function validarGender(e) {
+	if (genero.value == "" || genero.value == null) {
+		error.style.display = "block";
+		error.innerHTML += "<li> Select gender </li>";
+		console.log("Please select gender");
+		e.preventDefault();
+	} else {
+		error.style.display = "none";
+	}
 }
-function validarTerms(e){
-    if(terminos.checked == false){
-        error.style.display = 'block';
-        error.innerHTML+= '<li> Agree terms and conditions </li>';
-        console.log("Please agree terms & conditions");
-        e.preventDefault();
-    } else{
-        error.style.display='none';
-    }
-}
-
-function validarForm(e){
-
-    error.innerHTML = '';
-    validarName(e);
-    validarEmail(e);
-    validarGender(e);
-    validarTerms(e);
+function validarTerms(e) {
+	if (terminos.checked == false) {
+		error.style.display = "block";
+		error.innerHTML += "<li> Agree terms and conditions </li>";
+		console.log("Please agree terms & conditions");
+		e.preventDefault();
+	} else {
+		error.style.display = "none";
+	}
 }
 
-formulario.addEventListener('submit',validarForm);
+function validarFormulario(e) {
+	error.innerHTML = "";
+	validarName(e);
+	validarEmail(e);
+	validarGender(e);
+	validarTerms(e);
+}
+
+Formulario.addEventListener("submit", validarFormulario);
