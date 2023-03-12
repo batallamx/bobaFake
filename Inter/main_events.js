bef53d1f607372026f5c64d730e7e8b6e4401e2d
00Kique <124222443+00Kique@users.noreply.github.com>
2023-03-11 19:43:38 -0600

    segundos en pantalla

--- Inter/main_events.js
+++ Inter/main_events.js
@@ -7,7 +7,14 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  var today = new Date();
-  var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-  document.querySelector('.date').innerHTML = today.toLocaleDateString('es-ES', options);
+  var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour:'numeric', minute:'numeric', second:'numeric'};
+
+  function updateTime() {
+    var today = new Date();
+    document.querySelector('.date').innerHTML = today.toLocaleDateString('es-ES', options);
+  }
+
+  updateTime(); // Actualiza la pantalla la primera vez
+
+  setInterval(updateTime, 1000); // Actualiza la pantalla cada segundo
 });
